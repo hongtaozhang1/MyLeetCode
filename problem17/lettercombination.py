9dd01108bd06e2cb6ad746322105f5fdfa69356e
hongtaoZhang <zhang490@indiana.edu>
2015-08-21 22:44:33 -0400

    Problem17 (while loop for recursion)
    WOW!

--- problem17/lettercombination.py
+++ problem17/lettercombination.py
@@ -1,36 +1,70 @@
 __author__ = 'hongtao'
 class Solution:
-    num2char = [[" "], [""], ["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"], ["j", "k", "l"]
-                ,["m", "n", "o"], ["p", "q", "r", "s"], ["t", "u", "v"], [ "w", "x", "y", "z"]]
+    # num2char = [[" "], [""], ["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"], ["j", "k", "l"]
+    #             ,["m", "n", "o"], ["p", "q", "r", "s"], ["t", "u", "v"], [ "w", "x", "y", "z"]]
+    #
+    # def generateAns(self, digit, tmp, ans):
+    #     if digit == "":
+    #         ans.append(tmp)
+    #         return
+    #     for char in self.num2char[int(digit[0])]:
+    #         tmp += char
+    #         self.generateAns(digit[1:], tmp, ans)
+    #         tmp = tmp[0:len(tmp)-1]
+    #
+    # def letterCombinations(self, digit):
+    #     ans = []
+    #     tmp = ""
+    #     if digit == "":
+    #         return ans
+    #     self.generateAns(digit, tmp, ans)
+    #     return ans
 
-    def generateAns(self, digit, tmp, ans):
-        if digit == "":
-            ans.append(tmp)
-            return
-        for char in self.num2char[int(digit[0])]:
-            tmp += char
-            self.generateAns(digit[1:], tmp, ans)
-            tmp = tmp[0:len(tmp)-1]
 
     def letterCombinations(self, digit):
-        ans = []
-        tmp = ""
+        num2char = [[" "], [""], ["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"], ["j", "k", "l"]
+                    ,["m", "n", "o"], ["p", "q", "r", "s"], ["t", "u", "v"], ["w", "x", "z"]]
+
         if digit == "":
-            return ans
-        self.generateAns(digit, tmp, ans)
+            return []
+        stack = []
+        struct = {}
+        ans = []
+        struct["tmp"] = ""
+        struct["digit"] = digit
+        struct["stage"] = 0
+        struct["i"] = 0
+        stack.append(struct)
+        while len(stack) > 0:
+            currentStruct = stack.pop()
+            if currentStruct["digit"] == "":
+                ans.append(currentStruct["tmp"])
+                continue
+            if currentStruct["stage"] == 0:
+                currentStruct["stage"] = 1
+                stack.append(currentStruct)
+
+                for char in num2char[int(currentStruct["digit"][0])]:
+                    newStruct = {}
+                    newStruct["stage"] = 0
+                    newStruct["digit"] = currentStruct["digit"][1:]
+                    newStruct["tmp"] = currentStruct["tmp"] + char
+
+                    stack.append(newStruct)
+                continue
+            else:
+                currentStruct["tmp"] = currentStruct["tmp"][0:len(currentStruct["tmp"])-1]
+
         return ans
 
 
-    # def letterCombinations(self, digit):
-    #     num2char = [[" "], [""], ["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"], ["j", "k", "l"]
-    #                 ,["m", "n", "o"], ["p", "q", "r", "s"], ["t", "u", "v"], ["w", "x", "z"]]
-    #     ans = []
-    #
-    #     i = 0
-    #     while i < len(digit):
+
+
+
+
 
 
 solution = Solution()
-print solution.letterCombinations("23")
+print solution.letterCombinations("234")
 
 
